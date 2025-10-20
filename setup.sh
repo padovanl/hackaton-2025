5b47488d139420e779d60d0551cd8cc0993e8527
Luca Padovan <lpadovan@qubicaamf.com>
2025-10-21 00:36:45 +0200

    stable

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "❌ This script must be exec with 'source' or '.'."
+  echo "   Use: source $0"
+  return 1 2>/dev/null || exit 1
+fi
+
+
 if [ ! -d ".venv" ]; then
   echo "venv not found, create it..."
   python3 -m venv .venv
@@ -8,6 +15,7 @@ if [ ! -d ".venv" ]; then
   echo "Install requirements..."
   python -m pip install -r requirements.txt
 else
+  echo "venv exists, activate venv..."
   source .venv/bin/activate
 fi
 
